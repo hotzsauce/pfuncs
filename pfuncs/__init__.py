2ed8bf18fa7004b1ad44532210bfd8c555387bc5
hotzsauce <hotzlogan@gmail.com>
2020-05-21 15:04:06 -0500

    scrapped call() method; PFunc does all parsing

--- pfuncs/__init__.py
+++ pfuncs/__init__.py
@@ -1,23 +1,7 @@
-from pfuncs.lexer import Lexer
+
+from pfuncs.callable import PFunc
+from pfuncs.lexer import Lexer 
 from pfuncs.functions import (
 	Parser,
 	Interpreter
 )
-from pfuncs.semantics import SemanticAnalyzer
-
-
-def call(text):
-	lexer = Lexer(text)
-	parser = Parser(lexer)
-
-	tree = parser.parse()
-
-	sem_analyzer = SemanticAnalyzer(tree)
-	sem_analyzer.analyze()
-
-	interpreter = Interpreter(
-					tree=tree,
-					variables=sem_analyzer.variables
-	)
-
-	return interpreter.interpret()
